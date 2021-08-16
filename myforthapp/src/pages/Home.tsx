0a1fb42f82739f6716f9124e16dc1817eb280450
Binh Do <binhdq@fe.edu.vn>
2021-08-16 10:15:38 +0700

    more UI components

---
+++ myforthapp/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+         This is the home page
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
---
+++ myforthapp/src/pages/Register.tsx
@@ -0,0 +1,56 @@
+import { IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonPage, IonRadio, IonRadioGroup, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
+
+const Register: React.FC = () => {
+  const [name, setName] = useState('')
+  const [country, setCountry] = useState('')
+  const [gender,setGender] = useState('')
+  const [languages,setLanguages] = useState<string[]>()
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Register</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">Name</IonLabel>
+            <IonInput onIonChange={e => setName(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Country</IonLabel>
+            <IonSelect onIonChange={e => setCountry(e.detail.value)}>
+              <IonSelectOption value="Vietnam">Vietnam</IonSelectOption>
+              <IonSelectOption value="The USA">The USA</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Gender</IonLabel>
+            <IonRadioGroup onIonChange={e=>setGender(e.detail.value)}>
+              <IonItem lines="none">
+                <IonLabel>Male</IonLabel>
+                <IonRadio value="Male" />
+              </IonItem>
+              <IonItem lines="none">
+                <IonLabel>Female</IonLabel>
+                <IonRadio value="Female" />
+              </IonItem>
+            </IonRadioGroup>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Langagues can speak</IonLabel>
+            <IonSelect multiple={true} onIonChange={e=>setLanguages(e.detail.value)}>
+              <IonSelectOption value="Vietnamese">Vietnamese</IonSelectOption>
+              <IonSelectOption value="English">English</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+        </IonList>
+        {languages}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Register;
